3cfc0a9628b40c861b7669a666aff22a3b536ab5
HieuCR7 <hieuhv97@gmail.com>
2018-08-26 11:52:28 +0700

    frontend layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # folder are already added.
 Rails.application.config.assets.precompile += %w(*.jpg *.png)
 Rails.application.config.assets.precompile += %w(admin.js admin.scss)
+Rails.application.config.assets.precompile += %w(home.js home.scss)
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   namespace :admin do
     root "dashboard#index"
   end
+
+  root "home#index"
 end
